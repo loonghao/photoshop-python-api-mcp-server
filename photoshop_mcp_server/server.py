100eac721d4167d30639c4f1280da8def9e9f784
lingchenma <lichen218@qq.com>
2025-06-09 11:39:00 +0800

    Add log printing to the file

--- photoshop_mcp_server/server.py
+++ photoshop_mcp_server/server.py
@@ -4,7 +4,6 @@ import logging
 import os
 import sys
 from typing import Any
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from mcp.server.fastmcp import FastMCP
 
